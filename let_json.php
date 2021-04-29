9f002cb15da74d2b5733e5228accd6f6c77edc42
tom <tom@sapletta.com>
2021-04-29 21:10:13 +0200

    associative and callback

--- let_json.php
+++ let_json.php
@@ -11,6 +11,16 @@ function let_json($url, $callback = null, $associative = false)
     $file = file_get_contents($url, true);
     $json = json_decode($file, $associative);
 
+    if ($json === false) {
+        // Avoid echo of empty string (which is invalid JSON), and
+        // JSONify the error message instead:
+        $json = json_encode(["jsonError" => json_last_error_msg()]);
+        if ($json === false) {
+            // This should not happen, but we go all the way now:
+            $json = '{"jsonError":"unknown"}';
+        }
+    }
+
     if (is_callable($callback)) {
         $callback($json);
     }
